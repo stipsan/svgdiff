15a4c4df1918f874b349e51e6efbe90d23a4de3c
Cody Olsen <stipsan@gmail.com>
2021-04-18 17:10:26 +0000

    replacing linaria with tailwind

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
-const withCSS = require('@zeit/next-css')
-
-module.exports = withCSS({
-  webpack(config, options) {
-    const tsRule = config.module.rules.findIndex((loader) =>
-      loader.test.test('test.tsx')
-    )
-    // Dirty dirty dirty hack to make the zero runtime stuff work
-    config.module.rules[tsRule].use = [
-      ...[].concat(config.module.rules[tsRule].use),
-      {
-        loader: 'linaria/loader',
-        options: {
-          sourceMap: process.env.NODE_ENV !== 'production',
-        },
-      },
-    ]
-
-    return config
+module.exports = {
+  future: {
+    webpack5: true,
   },
-})
+}
